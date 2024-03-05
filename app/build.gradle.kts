1f6540104332dacd6f742baa9e55e9eb24c8f346
Muhammed <4d7568616d6d6564@gmail.com>
2024-03-05 16:34:24 +0300

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mkdevelopment.myaccounts"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.0.14"
+        versionCode = 15
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
